8604b391ba8230d2316bf0cbca6f0ef55cacba7d
bl4ckan4c0nda <bl4ckan4c0nda@gmail.com>
2023-02-03 21:22:59 +0330

    Add main.jsx for React app entry point and routing

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './components/auth/Login'
+import Register from './components/auth/Register';
+import App from './App'
+import './index.css'
+import './assets/flaticon/flaticon_mycollection.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  <RouterProvider router={router} />
+  // </React.StrictMode>,
+)
