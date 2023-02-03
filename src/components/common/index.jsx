d68df29c64db7834e10626e72c2185eb455338b8
bl4ckan4c0nda <bl4ckan4c0nda@gmail.com>
2023-02-03 21:10:36 +0330

    Add index.jsx for exporting components

---
+++ src/components/common/index.jsx
@@ -0,0 +1,8 @@
+import AppContainer from "../common/AppContainer"
+import Button from "../common/Button";
+import SeparatorOr from "../common/SeparatorOr";
+import TextLink from "../common/TextLink";
+import BackButton from "../common/BackButton";
+import AuthPageTitle from "../common/AuthPageTitle"
+
+export { AppContainer, Button, SeparatorOr, TextLink, BackButton, AuthPageTitle }
