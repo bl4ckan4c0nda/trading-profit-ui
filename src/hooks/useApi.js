0aad14321a896d957ef44215d5164db21ff4d1c0
bl4ckan4c0nda <bl4ckan4c0nda@gmail.com>
2023-03-06 08:34:43 +0000

    useeApi hook improved

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,23 +1,49 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+// import { useState, useEffect } from "react"
+// import axios from "axios"
 
-export function useApi(url) {
-    const [data, setData] = useState([])
-    const [isLoading, setIsloading] = useState(false)
-    const [error, setError] = useState(null)
+// export function useApi(url) {
+//     const [data, setData] = useState([])
+//     const [isLoading, setIsloading] = useState(false)
+//     const [error, setError] = useState(null)
+
+//     useEffect(() => {
+//         setIsloading(true)
+//         axios.get(url)
+//             .then(response => {
+//                 setData(response.data)
+//                 setIsloading(false)
+//             })
+//             .catch(error => {
+//                 setError(error)
+//                 setIsloading(false)
+//             })
+//     }, [url])
+
+//     return [data, isLoading, error]
+// }
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useApi = (url, method = "get") => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
 
     useEffect(() => {
-        setIsloading(true)
-        axios.get(url)
-            .then(response => {
-                setData(response.data)
-                setIsloading(false)
-            })
-            .catch(error => {
-                setError(error)
-                setIsloading(false)
-            })
-    }, [url])
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const response = await axios[method](url);
+                setData(response.data);
+            } catch (error) {
+                setError(error.message);
+            }
+            setIsLoading(false);
+        };
+        fetchData();
+    }, [url, method]);
 
-    return [data, isLoading, error]
-}
+    return { data, isLoading, error };
+};
+
+export default useApi;
